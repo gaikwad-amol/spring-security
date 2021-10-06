6e4e80e3f9640335e960e7484ad986c07241779f
Amol Gaikwad <amol@Amols-MacBook-Pro.local>
2021-10-06 10:51:39 +0530

    first commit

---
+++ src/main/java/com/amol/springsecurity/SpringSecurityApplication.java
@@ -0,0 +1,14 @@
+package com.amol.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityApplication {
+
+	// https://www.toptal.com/spring/spring-security-tutorial
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityApplication.class, args);
+	}
+
+}
